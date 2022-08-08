9a763f9ce77815da39d1de7b52c6179bb0e2badb
Eric <ekameyshaaa@gmail.com>
2022-08-08 11:07:31 +0300

    create Dialogs, react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,25 @@ import './App.css'
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter,Routes, Route} from "react-router-dom";
 
 const App = () => {
     return (
+        <BrowserRouter>
         <div className={'app-wrapper'}>
-          <Header/>
-           <NavBar />
-          <Profile />
+            <Header/>
+            <NavBar/>
+            <div className={'app-wrapper-content'}>
+                <Routes>
+                {/*<Dialogs/>*/}
+                    <Route path={"/dialog"} element={<Dialogs/>}/>
+                    <Route path={"/profile"} element={<Profile/>}/>
+                </Routes>
+            </div>
+
         </div>
+        </BrowserRouter>
     );
 };
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 import s from './NavBar.module.css'
-
+import {Link} from "react-router-dom";
 const NavBar = () => {
     return (
         <nav className={s.nav}>
 
-                <div className={`${s.item} ${s.active}`  }><a>profile</a></div>
-                <div className={s.item}><a>Message</a></div>
-                <div className={s.item}><a>main</a></div>
+                <div className={`${s.item} ${s.active}`  }><Link to={'/profile'}>profile</Link></div>
+                <div className={`${s.item} ${s.active}`  }><Link to={ '/dialog'}>Message</Link></div>
+                <div className={`${s.item} ${s.active}`  }><Link to={'*'} >main</Link></div>
         </nav>
     );
 };
